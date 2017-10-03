6734246bd016e9dc0ad04b6f761c556cd76a6fe3
mystiichawk <polmickael3@gmail.com>
2017-10-03 07:54:07 +0200

    correction d'erreur sur la requete sql et verification de connection de l'admin + js de verif

--- page/connectionAdmin.php
+++ page/connectionAdmin.php
@@ -1,74 +1,72 @@
-<?php 
+<?php
 
-// FUNCTION VERIFY IF THE PSEUDO AND PASSWORD ARE EMPTY 
-function testInput ($fichier) {
-	if (empty($_POST[$fichier])) {
-		return "<span class='text-danger'>" ."Le champ " .$fichier. " est vide <br>" . "</span>";
+function testInput($fichier){
+	if(empty($_POST['$fichier'])){
+		return "<div class='text-danger'> le champ ". $fichier . " est vide</div>";
 	}
 }
 
-if (isset($_POST['valid'])) {
-	
-	// //  INIT VAR NULL 
-	// $erreurPseudo = "";
-	// $erreurMdp = "";
 
-	//  VERIFY IF INPUT IS !EMPTY
-	$erreurPseudo = ": " .testInput('pseudo');
-	$erreurMdp = ": " .testInput('mot de passe');
-
-
-
-	if(empty($erreurPseudo && empty($erreurMdp))){
-
+	if(isset($_POST['valid'])) {
+		if (empty($_POST['pseudo'])) {
+			$erreurPseudo = ": " . testInput('pseudo');
+		} if (empty($_POST['password'])) {
+			$erreurMdp = ": " .testInput('mot de passe');
+		}
+		
+		if(!empty($_POST['pseudo']) && !empty($_POST['password'])) {
+		//Récupération et sécurisation des parametres
 		$pseudo = htmlspecialchars($_POST['pseudo'], ENT_QUOTES);
 		$password = htmlspecialchars($_POST['password'], ENT_QUOTES);
-
-			//CONNECTING AT BDD 
+		
+		//Vérification mot de passe
+		
+		//connecting at bdd 
 		include('./connect/connection.php');
+		// 	// 	request sql to verify correspondence of the user
+			$sql = sprintf("select uti_pseudo, uti_password from uti_utilisateur where uti_pseudo = '%s';", $pseudo);
+			
+			$reponse = $bdd->prepare($sql);
+			$reponse->execute();
+			
+			$row = $reponse->fetch();
 
-			// 	REQUEST SQL TO VERIFY CORRESPONDENCE OF THE USER
-		$sql = sprintf("SELECT * FROM uti_utilisateur WHERE uti_pseudo LIKE '%s';" , $pseudo);
-
-		$query = $bdd->prepare($sql);
-		$query->execute();
-		$donnees = $query->fetch();
-
-		if ($password = $donnees['uti_password']) {
-			session_start();
-			$_SESSION['uti_pseudo'] = $pseudo;
-			$_SESSION['uti_id'] = $donnees['uti_id'];
-			header('refresh:5;url=index.php?p=listArtAdmin');
-			$message = "<div class='container-fluid text-center'" . "<p><span class='text-success text-uppercase'><strong> Connection" . "<br>"."En tant qu'".$pseudo . "</strong></span></p>" ."<br> attendez 5 secondes ou cliquez sur ce <strong><a href='index.php?p=listArtAdmin'>lien</a></strong> pour être rediriger directement" ."</div>";
-			$erreurPseudo = "";
-			$erreurMdp = "";
-		} 
-		else {
-			$message = "<strong class='text-danger'>Identifient incorrect</strong>";
-		}
-	} 
+			if ($_POST['password'] === $row['uti_password']) {
+				session_start();
+				$_SESSION['uti_pseudo'] = $pseudo;
+				header('refresh:5;url=index.php?p=listArtAdmin');
+				$message = "<div class='container-fluid text-center'" . "<p><span class='text-success text-uppercase'><strong> Connection" . "<br>"."En tant qu'".$pseudo . "</strong></span></p>" ."<br> attendez 5 secondes ou cliquez sur ce <strong><a href='index.php?p=listArtAdmin'>lien</a></strong> pour être rediriger directement" ."</div>";
+			} else {
+				$message = "<strong class='text-danger'>Identifient incorrect</strong>";
+			}
+	}
 }
+
+
+
+
+
 
 
 ?>
 
-
 <div class="container">
 	<div class="row">
 		<div class="col-xs-5 col-xs-offset-3 jumbotron">
-			<form action="?p=connectionAdmin" method="post" id='form'>
+			<form method="post" id='form'>
 				<div class="form-group text-center" id="error1">
-					<label class="control-label" for="pseudo">Pseudonyme <i id="textPseudo" ></i><?= isset($erreurPseudo) ? $erreurPseudo: "" ?></label>
-					<input name="pseudo" type="text" class="champ form-control" id="pseudo">
+				<label for="pseudo">Pseudo <?= isset($erreurPseudo) ? $erreurPseudo: "" ?> <i id="textpseudo" ></i></label>
+				<input name="pseudo" type="text" class="champ form-control" id="pseudo">
 				</div>
 				<div class="form-group text-center" id="error2">
-					<label class="control-label" for="password">mot de passe <i id="textMdp" ></i> <?= isset($erreurMdp) ? $erreurMdp: ""  ?></label>
-					<input name="password" type="password" class="champ2 form-control" id="mdp">
+				<label for="mdp">Mot de passe <?= isset($erreurMdp) ? $erreurMdp: "" ?> <i id="textmdp" ></i></label>
+				<input name="password" type="password" class="champ2 form-control" id="mdp">
 				</div>
-				<input name="valid" class="btn btn-success pull-right" type="submit" value="Envoyer" />
+				<button class="btn btn-success btn-md pull-right" type="submit" id="envoi" name="valid">Validez</button>
+      	<input type="checkbox" id="passClair"> Affichez le mot de passe</label>
 			</form>
 			<p><?= isset($message) ? $message: "" ?></p>
-		</div>
+		</div>	
 	</div>
 </div>
 
--- page/js/loginAdmin.js
+++ page/js/loginAdmin.js
@@ -1,54 +1,56 @@
-$(document).ready(function(){
-
-	pseudo = $('#pseudo'),
-	mdp = $('#mdp'),
-	error1 = $("#error1"),
-	error2 = $('#error2'),
-	textpseudo = $('#textPseudo'),
-	textMdp = $('#textMdp'),
-
-
-	$("#form").submit(function(e){
-
-		error1.removeClass("has-error");
-		error2.removeClass("has-error");
-
- // PSEUDO
- 
-// validation ou erreur de saisie a modifier en fonction du PHP
-
-if (pseudo.val().length <5 || pseudo.val().length >45) {
-	error1.addClass("has-error");
-	textpseudo.html("<strong class='text-danger'> : minimum de 5 à 45 caractères</strong>");
-	e.preventDefault();
-
-}
-else { 
-	error1.removeClass("has-error"); 
-	error1.addClass("has-success");
-	textpseudo.remove();
-}
-
-//  MOT DE PASSE
-
-// validation ou erreur de saisie a modifier en fonction du PHP
-
-if (mdp.val().length <5 || mdp.val().length >25) {
-	error2.addClass("has-error");
-	textMdp.html("<strong class='text-danger'> : minimum de 5 à 45 caractères</strong>");
-	e.preventDefault();
-
-}
-else { 
-	error2.removeClass("has-error"); 
-	error2.addClass("has-success");
-	textMdp.remove();
-}
-
-
-});
-
+$(document).ready(function () {
 	
-});
-
-
+	
+	
+		pseudo = $('#pseudo'),
+			mdp = $('#mdp'),
+			error1 = $("#error1"),
+			error2 = $('#error2'),
+			textPseudo = $('#textpseudo'),
+			textmdp = $('#textmdp'),
+			passClair = $('#passClair'),
+	
+	
+			passClair.click(function(){
+				if (mdp.attr('type') === "password") {
+					mdp.attr('type', 'text');
+				} else {
+					mdp.attr('type', 'password');
+				}
+			})	
+	
+			$("#form").submit(function (e) {
+	
+				error1.removeClass("has-error");
+				error2.removeClass("has-error");
+	
+				// PSEUDO
+	
+				// validation ou erreur de saisie a modifier en fonction du PHP
+	
+				if (pseudo.val().length < 5 || pseudo.val().length > 45) {
+					error1.addClass("has-error");
+					textPseudo.html("<strong class='text-danger'><i class='glyphicon glyphicon-remove'></i> 5 caractères min</strong>");
+					e.preventDefault();
+	
+				} else {
+					error1.removeClass("has-error").addClass("has-success");
+					textPseudo.html("<strong class='text-success'><i class='glyphicon glyphicon-ok'></i></strong>");
+				}
+	
+				//  MOT DE PASSE
+	
+				// validation ou erreur de saisie a modifier en fonction du PHP
+	
+				if (mdp.val().length < 5 || mdp.val().length > 255) {
+					error2.addClass("has-error");
+					textmdp.html("<strong class='text-danger'><i class='glyphicon glyphicon-remove'></i> 5 caractères min</strong>");
+					e.preventDefault();
+				} else {
+					error2.removeClass("has-error").addClass("has-success");
+					textmdp.html("<strong class='text-success'><i class='glyphicon glyphicon-ok'></i></strong>");
+				}
+			});
+	
+	
+	});
